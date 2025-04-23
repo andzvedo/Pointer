6f84d8bb7452e949cca850d09825f6315889d52a
André Luiz de Azevedo <andreazeved1@gmail.com>
2025-04-22 22:19:24 -0300

    Remove arquivos desnecessários e configurações do projeto, incluindo .gitignore, .prettierrc, jest.config.js, package.json, e outros arquivos de configuração e código. Essa limpeza visa simplificar a estrutura do repositório e preparar para uma nova abordagem de desenvolvimento.

--- src/components/DynamicFigmaCanvasEditor.tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from 'next/dynamic'
-
-// Importar o FigmaCanvasEditor dinamicamente com SSR desativado
-const DynamicFigmaCanvasEditor = dynamic(
-  () => import('./FigmaCanvasEditor'),
-  { ssr: false }, // Desativa a renderização no lado do servidor
-)
-
-export default DynamicFigmaCanvasEditor
--- src/types/next.d.ts
+++
@@ -1,6 +0,0 @@
-// Declaração de tipos para next/server
-declare module 'next/server' {
-  export interface NextRequest extends Request {
-    nextUrl: URL;
-  }
-} 
